19dd37973b6b60eac634a0f6ba78ce8062232260
Getze-23 <yarelileyva2f@gmail.com>
2025-11-17 23:27:45 -0600

    Add memoization to Fibonacci function
    Implement memoization for Fibonacci calculation.

--- Practica 11/fibonacci.c
+++ Practica 11/fibonacci.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+
+int  long memoria[100];  
+int long fibonacci(int n)
+ {
+
+    if (n == 1) return 0;   // f1 = 0
+    if (n == 2) return 1;   // f2 = 1
+    if (memoria[n] != -1) {
+        return memoria[n];
+    }
+
+    memoria[n] = fibonacci(n - 1) + fibonacci(n - 2);
+
+    return memoria[n];
+}
+
+int main() {
+
+    for (int i = 0; i < 100; i++) {
+        memoria[i] = -1;
+    }
+
+    int posiciones[] = {7, 21, 40, 71, 94};
+    int total = 5;
+
+    printf("Valores de la secuencia de Fibonacci:\n\n");
+
+    for (int i = 0; i < total; i++) {
+        int pos = posiciones[i];
+        printf("Fibonacci(%d) = %lld\n", pos, fibonacci(pos));
+    }
+
+    printf("\n");
+
+    return 0;
+}
 
