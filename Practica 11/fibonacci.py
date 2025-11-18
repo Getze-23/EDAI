40abace8dfe2e2be442ca31fa52295827af23dde
Getze-23 <yarelileyva2f@gmail.com>
2025-11-17 23:32:18 -0600

    Update fibonacci.py

--- Practica 11/fibonacci.py
+++ Practica 11/fibonacci.py
@@ -1,1 +1,26 @@
 
+memoria = {1: 0, 2: 1}
+
+
+def fibonacci(n):
+    
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+
+    if n in memoria:
+        return memoria[n]
+
+    memoria[n] = fibonacci(n - 1) + fibonacci(n - 2)
+
+    return memoria[n]
+
+
+posiciones = [7, 21, 40, 71, 94]
+
+print("Valores de la secuencia de Fibonacci:\n")
+
+for p in posiciones:
+    print(f"Fibonacci({p}) =", fibonacci(p))
+
