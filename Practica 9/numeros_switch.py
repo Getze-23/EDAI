8f0fece673e89c6c946c0fab257e0aed27429724
Getze-23 <yarelileyva2f@gmail.com>
2025-11-20 20:23:05 -0600

    Implement number switch case function
    Fix syntax errors and improve number handling.

---
+++ Practica 9/numeros_switch.py
@@ -0,0 +1,12 @@
+def numeros (num):
+  if num ==1 :
+    print("tu numero es 1")
+  elif num==1
+          print("el numero es 2")
+  elif num==2
+  print("el numero es 3")
+elif num==3
+print("el numero es 4")
+elif num==3
+else:
+print("no hay opcion")
