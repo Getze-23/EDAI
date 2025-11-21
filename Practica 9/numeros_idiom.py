9d781a62f23ea0548c11ffebef8762f8f2e3a9ae
Getze-23 <yarelileyva2f@gmail.com>
2025-11-20 21:38:46 -0600

    Fix syntax errors and improve output messages

--- Practica 9/numeros_idiom.py
+++ Practica 9/numeros_idiom.py
@@ -1,5 +1,7 @@
-def numeros_idiom(num)
-if num in (1,2,3,4)
-print("tu numero es {}".format(num)
-      else:
-print("{} no es una opcion".format (num))
+def numeros_idiom(num):
+    if num in (1, 2, 3, 4):
+        print("Tu número es {}".format(num))
+    else:
+        print("{} no es una opción".format(num))
+numeros_idiom(3)
+numeros_idiom(7)
