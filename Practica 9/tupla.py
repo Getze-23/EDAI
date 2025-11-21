e01dfb058d22c1232dad8455fddb0d799a8bb4e7
Getze-23 <yarelileyva2f@gmail.com>
2025-11-20 21:49:18 -0600

    Fix import statement and improve print formatting

--- Practica 9/tupla.py
+++ Practica 9/tupla.py
@@ -1,9 +1,13 @@
-from collecions impor namedtupla
-planeta=namedtuple('planeta',['nombre','numero'])
-planeta1=planeta('Mercurio',1)
+from collections import namedtuple
+
+planeta = namedtuple('planeta', ['nombre', 'numero'])
+
+planeta1 = planeta('Mercurio', 1)
 print(planeta1)
 
-planeta2=planeta('Venus',2)
-print(planeta1.nombre,planeta1.numero)
-print(planeta2[0],planeta2[1])
-print('Campo de la upla:{}'.format(planeta1._fields))
+planeta2 = planeta('Venus', 2)
+
+print(planeta1.nombre, planeta1.numero)
+print(planeta2[0], planeta2[1])
+
+print('Campos de la tupla: {}'.format(planeta1._fields))
