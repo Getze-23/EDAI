2c1fcae6f21a465df2be627d47327b497733abdc
Getze-23 <yarelileyva2f@gmail.com>
2025-11-20 21:50:27 -0600

    Fix variable name and formatting in tupla_diasDelMes

--- Practica 9/tupla_diasDelMes.py
+++ Practica 9/tupla_diasDelMes.py
@@ -1,5 +1,6 @@
-tupla_diasDelMes=(31,28,31,30,31,30,31,31,30,31,30,31)
-print(tupla_diasDelMEs)
+tupla_diasDelMes = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+print(tupla_diasDelMes)
 print(tupla_diasDelMes[0])
 print(tupla_diasDelMes[3])
-print(upla_diasDelMes[1])
+print(tupla_diasDelMes[1])
