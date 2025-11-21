1e187b0c836625fbcdc2aedf753079f0b344ae1b
Getze-23 <yarelileyva2f@gmail.com>
2025-11-20 21:40:19 -0600

    Update numeros_switch.py

--- Practica 9/numeros_switch.py
+++ Practica 9/numeros_switch.py
@@ -1,12 +1,13 @@
-def numeros (num):
-  if num ==1 :
-    print("tu numero es 1")
-  elif num==1
-          print("el numero es 2")
-  elif num==2
-  print("el numero es 3")
-elif num==3
-print("el numero es 4")
-elif num==3
-else:
-print("no hay opcion")
+def numeros(num):
+    if num == 1:
+        print("Tu número es 1")
+    elif num == 2:
+        print("El número es 2")
+    elif num == 3:
+        print("El número es 3")
+    elif num == 4:
+        print("El número es 4")
+    else:
+        print("No hay opción")
+numeros(3)
+numeros(10)
