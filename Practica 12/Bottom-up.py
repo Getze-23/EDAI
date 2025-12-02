7ac028e39f7353f5d51a6570a7c83024fe8a3a33
Getze-23 <yarelileyva2f@gmail.com>
2025-12-02 17:17:33 -0600

    Add bottom-up Fibonacci function
    Implement bottom-up Fibonacci function and print series.

--- Practica 12/Bottom-up.py
+++ Practica 12/Bottom-up.py
@@ -1,1 +1,15 @@
+def fibonacci_bottom_up(n):
+    if n <= 1:
+        print([0] if n == 0 else [0,1])
+        return n
+
+    tabla = [0, 1]
+
+    for i in range(2, n + 1):
+        tabla.append(tabla[i-1] + tabla[i-2])
+
+    print("Tabla de serie fibonacci:\n", tabla)
+    return tabla[n]
+
+fibonacci_bottom_up(50)
 
