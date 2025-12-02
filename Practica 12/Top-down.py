4ed9cae2f6060bc9129a60319a00a6466f5692a8
Getze-23 <yarelileyva2f@gmail.com>
2025-12-02 17:20:53 -0600

    Add top-down Fibonacci function
    Implement top-down Fibonacci function with memoization.

--- Practica 12/Top-down.py
+++ Practica 12/Top-down.py
@@ -1,1 +1,18 @@
+def fibonacci_top_down(n, memo=None):
+    if memo is None:
+        memo = {}
+
+    if n in memo:
+        return memo[n]
+
+    if n <= 1:
+        memo[n] = n
+        return n
+
+    memo[n] = fibonacci_top_down(n-1, memo) + fibonacci_top_down(n-2, memo)
+    return memo[n]
+
+
+# Ejemplo:
+print("Top-down:", fibonacci_top_down(1))
 
