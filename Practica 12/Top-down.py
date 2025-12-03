b38dc55e68377bb48fae153d1b4b75a5f4b0de6e
Getze-23 <yarelileyva2f@gmail.com>
2025-12-02 21:30:45 -0600

    Enhance fibonacci_top_down with debug output
    Added debugging prints to track Fibonacci calculations and memoization.

--- Practica 12/Top-down.py
+++ Practica 12/Top-down.py
@@ -1,17 +1,23 @@
-def fibonacci_top_down(n, memo=None):
+def fibonacci_top_down(n, memo=None, nivel=0):
     if memo is None:
         memo = {}
 
+    sangria = "   " * nivel
+
     if n in memo:
+        print(f"{sangria}f({n}) ya está en  la memoria → {memo[n]}")
         return memo[n]
 
     if n <= 1:
         memo[n] = n
+        print(f"{sangria}Guardando: f({n}) = {n}")
         return n
 
-    memo[n] = fibonacci_top_down(n-1, memo) + fibonacci_top_down(n-2, memo)
+    print(f"{sangria}Calculando f({n}) → f({n-1}) + f({n-2})")
+    memo[n] = fibonacci_top_down(n-1, memo, nivel+1) + fibonacci_top_down(n-2, memo, nivel+1)
+
+    print(f"{sangria}Guardando: f({n}) = {memo[n]}")
     return memo[n]
 
-
-print("Top-down:", fibonacci_top_down(1))
+fibonacci_top_down(10)
 
